17016a971284adaaea181ae637a1218fc175857d
Calvin Phuah <zhiyuan.phuah@gamuda.com.my>
2024-12-16 15:29:47 +0800

    classes

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,16 @@
 "use strict";
-let age = 30;
-console.log("hey");
+function printPosts(posts) {
+    for (let post of posts) {
+        console.log(post.title);
+        console.log(post.body);
+        console.log(post.id);
+    }
+}
+const posts = [
+    {
+        title: "Hello World",
+        body: "This is the body",
+        id: 1,
+    },
+];
+printPosts(posts);
